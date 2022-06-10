9a94db83d74650a493b84758c02f96a6b814c311
Khaleel Jaber <98000000+xaleel@users.noreply.github.com>
2022-06-10 21:30:18 +0300

    Added tip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,15 +39,16 @@ function App() {
       } else {
         setGuesses(guesses.concat(guess))
         axios.get(`/solve/${g}`)
-        .then((res) => setWords(res.data.s))
-        .catch((err) => console.log(err));
+          .then((res) => setWords(res.data.s))
+          .catch((err) => console.log(err));
       }
-    }
-    document.querySelectorAll('.letter').forEach(i => {
-      i.className = 'letter'
-      i.value = ''
-      document.getElementById(`b${i.id.slice(1)}`).className = 'lb'
-    })
+      // Reset the Word component
+      document.querySelectorAll('.letter').forEach(i => {
+        i.className = 'letter'
+        i.value = ''
+        document.getElementById(`b${i.id.slice(1)}`).className = 'lb'
+      })
+    } 
   }
 
   const reset = () =>{
@@ -60,17 +61,34 @@ function App() {
     })
   }
 
+  const dismiss = event => {
+    document.getElementById('tip').className = 'no-dsp'
+  }
+
+  const insert = word => {
+    document.getElementById('l1').value = word.slice(0, 1)
+    document.getElementById('l2').value = word.slice(1, 2)
+    document.getElementById('l3').value = word.slice(2, 3)
+    document.getElementById('l4').value = word.slice(3, 4)
+    document.getElementById('l5').value = word.slice(4, 5)
+    document.getElementById('l1').focus()
+  }
+
   return (
     <>
-    <h1 className='title'>Wordle solver</h1>
+    <h1 className='title'>Wordle solv3r</h1>
     <div className='main'>
       <div className='left section'>
         <h2>Top suggestions:</h2>
-        <Suggestions words={words}/>
+        <Suggestions words={words} insert={insert}/>
       </div>
       <div className='right section'>
         <h2>Enter word</h2>
         <Word go={go}/>
+        <div className='tip' onClick={dismiss} id='tip'>
+          <p>Click the buttons to change the color or use the arrow keys while typing</p>
+          <p>(click to dismiss)</p>
+        </div>
         <Done guesses={guesses}/>
       </div>
     </div>
