84db6d03ff22b8daf2ae4749cdf94bf28f6196e5
Khaleel Jaber <98000000+xaleel@users.noreply.github.com>
2022-06-10 21:32:04 +0300

    Added arrow feature, general improvements

--- frontend/src/Word.js
+++ frontend/src/Word.js
@@ -1,11 +1,17 @@
+import React from 'react'
+
 export default function Word(props){
     const focus = event => {
         if (event.type === 'change'){
             if (event.target.id !== 'l5'){event.target.nextSibling.focus()}
         }
         else {
-            let value = event.target.value.match(/[a-zA-Z]/) ? event.target.value : ''
-            if (event.code.search(/Key/) !== -1){
+            let value = event.target.value.match(/[a-zA-Z]/) ? event.target.value : '';
+            if (event.code === 'ArrowDown' || event.code === 'ArrowUp'){
+                let newClickEvent = new Event('click')
+                Object.defineProperty(newClickEvent, 'target', {writable: false, value: document.getElementById(`b${event.target.id.slice(1)}`)});
+                color(newClickEvent, event.code.toLowerCase().slice(5))
+            } else if (event.code.search(/Key/) !== -1){
                 event.target.value = event.code.slice(3)
                 if (event.target.id !== 'l5'){event.target.nextSibling.focus()}
             } else if (event.code === 'Backspace') {
@@ -26,46 +32,42 @@ export default function Word(props){
         }
     }
 
-    const color = event => {
+    const color = (event, key='up') => {
         if (event.target.classList.contains('gray')){
-            event.target.classList.remove('gray')
-            event.target.classList.add('yellow')
-            document.getElementById(`l${event.target.id.slice(1)}`).classList.remove('gray')
-            document.getElementById(`l${event.target.id.slice(1)}`).classList.add('yellow')
+            event.target.classList.remove('gray') // The button itself
+            event.target.classList.add(key == 'up' ? 'yellow' : 'green')
+            document.getElementById(`l${event.target.id.slice(1)}`).classList.remove('gray') // The input right above it
+            document.getElementById(`l${event.target.id.slice(1)}`).classList.add(key == 'up' ? 'yellow' : 'green')
         } else if (event.target.classList.contains('yellow')){
             event.target.classList.remove('yellow')
-            event.target.classList.add('green')
+            event.target.classList.add(key == 'up' ? 'green' : 'gray')
             document.getElementById(`l${event.target.id.slice(1)}`).classList.remove('yellow')
-            document.getElementById(`l${event.target.id.slice(1)}`).classList.add('green')
+            document.getElementById(`l${event.target.id.slice(1)}`).classList.add(key == 'up' ? 'green' : 'gray')
         } else if (event.target.classList.contains('green')){
             event.target.classList.remove('green')
-            event.target.classList.add('gray')
+            event.target.classList.add(key == 'up' ? 'gray' : 'yellow')
             document.getElementById(`l${event.target.id.slice(1)}`).classList.remove('green')
-            document.getElementById(`l${event.target.id.slice(1)}`).classList.add('gray')
+            document.getElementById(`l${event.target.id.slice(1)}`).classList.add(key == 'up' ? 'gray' : 'yellow')
         } else {
-            event.target.classList.add('gray')
-            document.getElementById(`l${event.target.id.slice(1)}`).classList.add('gray')
+            event.target.classList.add(key == 'up' ? 'gray' : 'green')
+            document.getElementById(`l${event.target.id.slice(1)}`).classList.add(key == 'up' ? 'gray' : 'green')
         }
     }
-    
+
     return (
-        <>
         <div className="word">
-            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l1'></input>
-            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l2'></input>
-            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l3'></input>
-            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l4'></input>
-            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l5'></input>
-            <button className="go" onClick={props.go}>&gt;</button>
+            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l1' />
+            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l2' />
+            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l3' />
+            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l4' />
+            <input className="letter" maxLength={1} pattern={/[a-zA-Z]/} onKeyUp={focus} id='l5' />
+            <input className="go" type="button" onClick={props.go} value="&gt;" />
+            <input className="lb" type="button" id="b1" onClick={color} />
+            <input className="lb" type="button" id="b2" onClick={color} />
+            <input className="lb" type="button" id="b3" onClick={color} />
+            <input className="lb" type="button" id="b4" onClick={color} />
+            <input className="lb" type="button" id="b5" onClick={color} />
+            <input className="lb xtra" id="b6"></input>
         </div>
-        <div className="buttons">
-                <button className="lb" id="b1" onClick={color}></button>
-                <button className="lb" id="b2" onClick={color}></button>
-                <button className="lb" id="b3" onClick={color}></button>
-                <button className="lb" id="b4" onClick={color}></button>
-                <button className="lb" id="b5" onClick={color}></button>
-                <button className="lb xtra" id="b6"></button>
-        </div>
-        </>
     )
 }
